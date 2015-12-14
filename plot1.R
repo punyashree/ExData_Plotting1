6b1936bdfde6b68de8a02e6bcd7a8566408fbebc
punyashree <punyashree.pb@compegence.com>
2015-12-14 00:25:02 +0000

    Project 1 files

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Plot 1
+
+# read from the source
+dataFile <- "D:/Data Science Track/Notes/Exploratory_DA/ExData_Plotting1/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+# subsetting
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off() 
---
+++ plot2.R
@@ -0,0 +1,17 @@
+#Plot 2
+#Reading Data
+dataFile <- "D:/Data Science Track/Notes/Exploratory_DA/ExData_Plotting1/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#Subsetting Required Data
+
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+#Creating DateTime Variable and converting 
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+#Plot 2
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off() 
